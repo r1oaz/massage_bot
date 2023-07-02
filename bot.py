c56af0bf15bb93be0a8117dfbcfa1cc1ac38b6ae
r1oaz <r1oaz@yandex.ru>
2023-07-02 20:27:40 +0300

    modified:   bot.py

--- bot.py
+++ bot.py
@@ -2,17 +2,12 @@ import telebot
 from config import Token
 from time import sleep
 bot = telebot.TeleBot(Token)
-while True:
-    print('starting...')
-    try:
-        bot.polling(none_stop=True, interval=0)
-    except KeyboardInterrupt:
-        with open("exit.log", "w", encoding="UTF-8") as f:
-            f.write("exited!")
-        exit()
-    except telebot.apihelper.ApiException:
-        print('Проверьте связь и API')
-        sleep(10)
-    except Exception as e:
-        print(e)
-        sleep(15)
+print('starting...')
+try:
+    bot.polling(none_stop=True, interval=0)
+except telebot.apihelper.ApiException:
+    print('Проверьте связь и API')
+    sleep(10)
+except Exception as e:
+    print(e)
+    sleep(15)
