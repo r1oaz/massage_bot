82089fe1f12b9f8c0c0779dcd38a99bbde25bad0
r1oaz <r1oaz@yandex.ru>
2023-07-27 11:53:00 +0300

    modified:   .gitignore modified:   db.xlsx modified:   reg_mas.py добавил проверку на прошедшую дату, т.е. теперь пользователь не сможет записаться на вчера, прошлую неделю, на прошлый год. так же пользователь не сможет записаться на выходной день

--- reg_mas.py
+++ reg_mas.py
@@ -28,15 +28,39 @@ def ask_date(message, fio, phone_number):
     bot.register_next_step_handler(message, ask_time, fio, phone_number, massage_type)
 
 def ask_time(message, fio, phone_number, massage_type):
-    # Проверяем введенную дату на корректность и предлагаем выбрать время
     date = message.text
+
+    if not is_valid_date(date):
+        bot.send_message(message.chat.id, "Вы ввели неверную дату. Введите дату начиная с сегодняшней.")
+        bot.register_next_step_handler(message, ask_time, fio, phone_number, massage_type)
+        return
+
+    entered_date = datetime.datetime.strptime(date, "%d.%m.%Y").date()
+    today = datetime.date.today()
+
+    if entered_date < today:
+        bot.send_message(message.chat.id, "На прошедшую дату запись невозможна.")
+        bot.register_next_step_handler(message, ask_time, fio, phone_number, massage_type)
+        return
+
+    if is_weekend(entered_date):
+        bot.send_message(message.chat.id, "Введённая дата является выходным днем. Запишитесь на будний день.")
+        bot.register_next_step_handler(message, ask_time, fio, phone_number, massage_type)
+        return
+
     if check_date_format(date):
-        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
         available_times = get_available_times(date)
-        for time in available_times:
-            keyboard.add(time)
-        bot.send_message(message.chat.id, "Выберите удобное время:", reply_markup=keyboard)
-        bot.register_next_step_handler(message, confirm_registration, fio, phone_number, massage_type, date)
+
+        if available_times:
+            keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            for time in available_times:
+                keyboard.add(time)
+
+            bot.send_message(message.chat.id, "Выберите удобное время:", reply_markup=keyboard)
+            bot.register_next_step_handler(message, confirm_registration, fio, phone_number, massage_type, date)
+        else:
+            bot.send_message(message.chat.id, "К сожалению, нет свободного времени на выбранную дату. Выберите другой день.")
+            bot.register_next_step_handler(message, ask_time, fio, phone_number, massage_type)
     else:
         bot.send_message(message.chat.id, "Некорректный формат даты. Введите дату (в формате дд.мм.гггг):")
         bot.register_next_step_handler(message, ask_time, fio, phone_number, massage_type)
@@ -72,28 +96,36 @@ def save_registration(message, fio, phone_number, massage_type, date, time_slot)
         main_menu(message)
 
     else:
-        register_massage(bot, message)
+        register_massage(message)
 
+def get_available_times(date):
+    wb = openpyxl.load_workbook('db.xlsx')
+    sheet = wb.active
+    times = set()
+
+    for row in sheet.iter_rows(values_only=True):
+        if row[3] == date:
+            times.add(row[4])
+
+    all_times = {'09:30', '10:00', '10:30', '11:00', '11:30', '12:00', '13:00', '13:30', '14:00', '14:30', '15:00', '15:30', '16:00', '16:30'}
+    available_times = all_times - times
+
+    return available_times
+
+
+def is_valid_date(date):
+    try:
+        datetime.datetime.strptime(date, "%d.%m.%Y")
+        return True
+    except ValueError:
+        return False
+
+def is_weekend(date):
+    return date.weekday() >= 5
 
 def check_date_format(date):
-    # Проверяем корректность формата даты
     try:
         datetime.datetime.strptime(date, '%d.%m.%Y')
         return True
     except ValueError:
         return False
-
-def get_available_times(date):
-    wb = openpyxl.load_workbook('db.xlsx')
-    sheet = wb.active
-    times = []
-
-    for row in sheet.iter_rows(values_only=True):
-        if row[3] == date:
-            times.append(row[4])
-
-    all_times = ['09:30', '10:00', '10:30', '11:00', '11:30', '12:00', '13:00', '13:30', '14:00', '14:30', '15:00', '15:30', '16:00', '16:30']
-
-    available_times = [time for time in all_times if time not in times]
-
-    return available_times
